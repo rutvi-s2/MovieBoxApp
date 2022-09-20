435a0271a20c474f3b1dc45b3dd0523cd530df18
rutvi-s2 <rutvi01.shah@gmail.com>
2022-09-20 13:19:33 -0500

    added binding var

--- app/src/main/java/com/example/moviebox/MainActivity.kt
+++ app/src/main/java/com/example/moviebox/MainActivity.kt
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.content.Intent
+import com.example.moviebox.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
 
         val secondPage = findViewById<Button>(R.id.movielistbtn)
