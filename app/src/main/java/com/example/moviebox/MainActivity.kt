a0c235c3f098c0ab6fd6ca6cf86bee29d828e771
rutvi-s2 <rutvi01.shah@gmail.com>
2022-10-09 17:30:10 -0500

    made import change

--- app/src/main/java/com/example/moviebox/MainActivity.kt
+++ app/src/main/java/com/example/moviebox/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.ImageDecoder
-import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
